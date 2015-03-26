6e4724fd44da609dbc637ec3834de3ec6fda228f
Denis Manilo <denis@binidini.com>
2015-03-26 12:30:06 +0300

    Bid crud added

--- src/Binidini/CoreBundle/Form/Type/BidType.php
+++ src/Binidini/CoreBundle/Form/Type/BidType.php
@@ -13,34 +13,25 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
-class ShippingType extends AbstractType
+class BidType extends AbstractType
 {
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('description')
-            ->add('pickupAddress')
-            ->add('deliveryAddress')
-            ->add('pickupDatetime', 'datetime', ['widget' =>'single_text', 'format' => 'dd.MM.yy HH:mm'])
-            ->add('deliveryDatetime', 'datetime', ['widget' =>'single_text', 'format' => 'dd.MM.yy HH:mm'])
-            ->add('deliveryPrice')
-            ->add('guaranteeCost')
-            ->add('weight')
-            ->add('x')
-            ->add('y')
-            ->add('z');
+            ->add('comment', 'textarea')
+            ->add('shipping', 'entity_hidden', ['data_class' => 'Binidini\CoreBundle\Entity\Shipping']);
     }
 
     public function setDefaultOptions(OptionsResolverInterface $resolver)
     {
         $resolver->setDefaults(array(
-            'data_class' => 'Binidini\CoreBundle\Entity\Shipping',
+            'data_class' => 'Binidini\CoreBundle\Entity\Bid',
         ));
     }
 
     public function getName()
     {
-        return 'binidini_core_shipping';
+        return 'binidini_core_bid';
     }
 }

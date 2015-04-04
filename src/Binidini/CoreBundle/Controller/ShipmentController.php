c69caf4a7c96bf0a5df62017e7cb7e7e7e0f4a32
Denis Manilo <denis@binidini.com>
2015-04-04 07:09:54 +0300

    add number of shipments

---
+++ src/Binidini/CoreBundle/Controller/ShipmentController.php
@@ -0,0 +1,27 @@
+<?php
+/*
+ * This file is part of the Binidini project.
+ *
+ * (c) Denis Manilo
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+/*
+ * This file is part of the Binidini project.
+ *
+ * (c) Denis Manilo
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Binidini\CoreBundle\Controller;
+
+
+use Sylius\Bundle\ResourceBundle\Controller\ResourceController;
+
+class ShipmentController  extends ResourceController
+{
+
+}

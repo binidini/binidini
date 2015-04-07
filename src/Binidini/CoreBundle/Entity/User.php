e149d7703140bbf36eba69c13c123b11314e4788
a.golovin <a.golovin@iconto.net>
2015-04-07 21:02:33 +0300

    Set address field as nullable

--- src/Binidini/CoreBundle/Entity/User.php
+++ src/Binidini/CoreBundle/Entity/User.php
@@ -113,7 +113,7 @@ class User extends BaseUser
     /**
      * @var string
      *
-     * @ORM\Column(name="address", type="string", length=255)
+     * @ORM\Column(name="address", type="string", length=255, nullable=true)
      */
     private $address;
 

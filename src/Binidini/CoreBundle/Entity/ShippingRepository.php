d9fbfa5b95fbc1d57d1847ebcb983db980bb0540
a.golovin <a.golovin@iconto.net>
2015-04-29 21:39:19 +0300

    Add: - search form for shipping in administration - add repository for shipping - add select by arguments in shipping

---
+++ src/Binidini/CoreBundle/Entity/ShippingRepository.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Binidini\CoreBundle\Entity;
+
+use Sylius\Bundle\ResourceBundle\Doctrine\ORM\EntityRepository;
+
+class ShippingRepository extends EntityRepository
+{
+
+    public function findByArguments($id, $state, $deliveryPriceFrom, $deliveryPriceTo, $insurancePriceFrom, $insurancePriceTo, $deliveryTimeFrom, $deliveryTimeTo)
+    {
+
+        $queryBuilder = $this->getCollectionQueryBuilder();
+        if ($id) {
+            $queryBuilder->andWhere($queryBuilder->expr()->eq($this->getPropertyName('id'), ':id'))
+                ->setParameter(':id', $id);
+        }
+        if ($state) {
+            $queryBuilder->andWhere($queryBuilder->expr()->eq($this->getPropertyName('state'), ':st'))
+                ->setParameter(':st',$state);
+
+        }
+        if ($deliveryPriceFrom) {
+            $queryBuilder->andWhere($queryBuilder->expr()->gte($this->getPropertyName('deliveryPrice'), ':dpf'))
+                ->setParameter(':dpf', $deliveryPriceFrom);
+        }
+        if ($deliveryPriceTo) {
+            $queryBuilder->andWhere($queryBuilder->expr()->lte($this->getPropertyName('deliveryPrice'), ':dpt'))
+                ->setParameter(':dpt', $deliveryPriceTo);
+        }
+        if ($insurancePriceFrom) {
+            $queryBuilder->andWhere($queryBuilder->expr()->gte($this->getPropertyName('insurance'), ':inf'))
+                ->setParameter(':inf', $insurancePriceFrom);
+        }
+        if ($insurancePriceTo) {
+            $queryBuilder->andWhere($queryBuilder->expr()->lte($this->getPropertyName('insurance'), ':int'))
+                ->setParameter(':int', $insurancePriceTo);
+        }
+        if ($deliveryTimeFrom) {
+            $queryBuilder->andWhere($queryBuilder->expr()->gte($this->getPropertyName('deliveryDatetime'), ':dtf'))
+                ->setParameter(':dtf', $deliveryTimeFrom);
+        }
+        if ($deliveryTimeTo) {
+            $queryBuilder->andWhere($queryBuilder->expr()->lte($this->getPropertyName('deliveryDatetime'), ':dtt'))
+                ->setParameter(':dtt', $deliveryTimeTo);
+        }
+
+        return $this->getPaginator($queryBuilder);
+    }
+}

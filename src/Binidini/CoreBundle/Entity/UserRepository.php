bbc72182c7cc314e69f6cb92eef8efde32f111f5
a.golovin <a.golovin@iconto.net>
2015-05-05 13:14:53 +0300

    Add: -search form for users in administration page

---
+++ src/Binidini/CoreBundle/Entity/UserRepository.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Binidini\CoreBundle\Entity;
+
+use Sylius\Bundle\ResourceBundle\Doctrine\ORM\EntityRepository;
+
+class UserRepository extends EntityRepository
+{
+
+    public function findByArguments($phone, $lastName, $firstName, $parentName, $email, $registrationFrom, $registrationTo, $countOfCarriers, $countOfSenders)
+    {
+        $queryBuilder = $this->getCollectionQueryBuilder();
+
+        if ($phone) {
+            $queryBuilder->andWhere($queryBuilder->expr()->eq($this->getPropertyName('username'), ':un'))
+                ->setParameter(':un', $phone);
+        }
+        if ($lastName) {
+            $queryBuilder->andWhere($queryBuilder->expr()->eq($this->getPropertyName('lastName'), ':ln'))
+                ->setParameter(':ln',$lastName);
+
+        }
+        if ($firstName) {
+            $queryBuilder->andWhere($queryBuilder->expr()->eq($this->getPropertyName('firstName'), ':fn'))
+                ->setParameter(':fn', $firstName);
+        }
+        if ($parentName) {
+            $queryBuilder->andWhere($queryBuilder->expr()->eq($this->getPropertyName('patronymic'), ':pn'))
+                ->setParameter(':pn', $parentName);
+        }
+        if ($email) {
+            $queryBuilder->andWhere($queryBuilder->expr()->eq($this->getPropertyName('email'), ':em'))
+                ->setParameter(':em', $email);
+        }
+        /*if ($registrationFrom) {
+            $queryBuilder->andWhere($queryBuilder->expr()->lte($this->getPropertyName('registrationFrom'), ':rgf'))
+                ->setParameter(':rgf', $registrationFrom);
+        }
+        if ($registrationTo) {
+            $queryBuilder->andWhere($queryBuilder->expr()->gte($this->getPropertyName('registrationTo'), ':rgt'))
+                ->setParameter(':rgt', $registrationTo);
+        }*/
+
+        /*if ($countOfCarriers) {
+            $queryBuilder->andWhere($queryBuilder->expr()->eq($this->getPropertyName('deliveryDatetime'), ':dtt'))
+                ->setParameter(':dtt', $countOfCarriers);
+        }*/
+
+        return $this->getPaginator($queryBuilder);
+    }
+}

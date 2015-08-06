e17df18bb8818810f5c8c94117cb37c8b8cd8ea7
Denis Manilo <denis@binidini.com>
2015-08-06 10:40:50 +0300

    github webhook done

--- src/Binidini/ApiBundle/Controller/GithubController.php
+++ src/Binidini/ApiBundle/Controller/GithubController.php
@@ -1,4 +1,5 @@
 <?php
+#https://github.com/betacie/webhooks
 
 namespace Binidini\ApiBundle\Controller;
 
@@ -10,6 +11,14 @@ class GithubController extends Controller
 {
     public function pushAction(Request $request)
     {
+        chdir('/var/www/tytymyty');
+
+        system('/usr/bin/env -i /usr/bin/git pull origin develop 2>&1');
+        system('/usr/bin/env -i php ./app/console doctrine:schema:update --force');
+        system('/usr/bin/env -i php ./app/console assets:install');
+        system('/usr/bin/env -i php ./app/console cache:clear --env=prod');
+        system('/usr/bin/env -i php ./app/console cache:clear');
+
         return new JsonResponse(['status' => 'ok']);
     }
 }

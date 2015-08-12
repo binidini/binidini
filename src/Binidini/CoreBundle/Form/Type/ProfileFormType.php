4fcbc34720db36101854a412e2f4c172300f5484
Alexander Golovin <allnimator@yandex.ru>
2015-08-12 18:53:35 +0300

    Fix update fields while edit user

--- src/Binidini/CoreBundle/Form/Type/ProfileFormType.php
+++ src/Binidini/CoreBundle/Form/Type/ProfileFormType.php
@@ -23,80 +23,80 @@ class ProfileFormType extends \FOS\UserBundle\Form\Type\ProfileFormType
 
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-
         $builder
-            ->add('email', 'email', ['required' => false])
             ->add('firstName')
             ->add('lastName')
-            ->add('patronymic')
-            ->add('companyName')
-            ->add('address')
-            ->add('type', 'choice',
-                [
-                    'choices' => [
-                        User::TYPE_INDIVIDUAL => 'Физическое лицо',
-                        User::TYPE_BUSINESS => 'Юридическое лицо',
-                    ],
-                    'multiple' => false,
-                    'expanded' => true,
-                    'required' => true,
-                ]
-            )
-            ->add('profileType', 'choice',
-                [
-                    'choices' => [
-                        User::PROFILE_TYPE_CARRIER_AND_SENDER => 'Отправитель и перевозчик',
-                        User::PROFILE_TYPE_CARRIER => 'Перевозчик',
-                        User::PROFILE_TYPE_SENDER => 'Отправитель',
-
-                    ],
-                    'multiple' => false,
-                    'expanded' => true,
-                    'required' => true,
-                ]
-            )
-            ->add('aboutMe', 'textarea', ['required' => false])
-            ->add('imgPath', 'file', ['required' => false, 'data_class' => null, 'mapped' => true])
-            // Блок настроек оповещения
-            ->add('smsBidCreateNotification', 'checkbox', ['required' => false])
-            ->add('smsBidAcceptNotification', 'checkbox', ['required' => false])
-            ->add('smsBidAgreeNotification', 'checkbox', ['required' => false])
-            ->add('smsBidRejectNotification', 'checkbox', ['required' => false])
-            ->add('smsBidRecallNotification', 'checkbox', ['required' => false])
-            ->add('smsShippingDeliverNotification', 'checkbox', ['required' => false])
-            ->add('smsShippingPayNotification', 'checkbox', ['required' => false])
-            ->add('smsShippingCompleteNotification', 'checkbox', ['required' => false])
-            ->add('smsShippingRefuseNotification', 'checkbox', ['required' => false])
-            ->add('smsShippingDisputeNotification', 'checkbox', ['required' => false])
-            ->add('smsShippingDebateNotification', 'checkbox', ['required' => false])
-            ->add('emailBidCreateNotification', 'checkbox', ['required' => false])
-            ->add('emailBidAcceptNotification', 'checkbox', ['required' => false])
-            ->add('emailBidAgreeNotification', 'checkbox', ['required' => false])
-            ->add('emailBidRejectNotification', 'checkbox', ['required' => false])
-            ->add('emailBidRecallNotification', 'checkbox', ['required' => false])
-            ->add('emailShippingDeliverNotification', 'checkbox', ['required' => false])
-            ->add('emailShippingPayNotification', 'checkbox', ['required' => false])
-            ->add('emailShippingCompleteNotification', 'checkbox', ['required' => false])
-            ->add('emailShippingRefuseNotification', 'checkbox', ['required' => false])
-            ->add('emailShippingDisputeNotification', 'checkbox', ['required' => false])
-            ->add('emailShippingDebateNotification', 'checkbox', ['required' => false])
-            ->add('gcmBidCreateNotification', 'checkbox', ['required' => false])
-            ->add('gcmBidAcceptNotification', 'checkbox', ['required' => false])
-            ->add('gcmBidAgreeNotification', 'checkbox', ['required' => false])
-            ->add('gcmBidRejectNotification', 'checkbox', ['required' => false])
-            ->add('gcmBidRecallNotification', 'checkbox', ['required' => false])
-            ->add('gcmShippingDeliverNotification', 'checkbox', ['required' => false])
-            ->add('gcmShippingPayNotification', 'checkbox', ['required' => false])
-            ->add('gcmShippingCompleteNotification', 'checkbox', ['required' => false])
-            ->add('gcmShippingRefuseNotification', 'checkbox', ['required' => false])
-            ->add('gcmShippingDisputeNotification', 'checkbox', ['required' => false])
-            ->add('gcmShippingDebateNotification', 'checkbox', ['required' => false]);
+            ->add('patronymic');
         if ($this->routeName == "binidini_api_user_update") {
             $builder
                 ->add('imgBase64', 'textarea', ['required' => false, 'data_class' => null, 'mapped' => true])
                 ->add('fileName', 'textarea', ['required' => false, 'data_class' => null, 'mapped' => true]);
-        }
+        } else {
+            $builder
+                ->add('email', 'email', ['required' => false])
+                ->add('companyName')
+                ->add('address')
+                ->add('type', 'choice',
+                    [
+                        'choices' => [
+                            User::TYPE_INDIVIDUAL => 'Физическое лицо',
+                            User::TYPE_BUSINESS => 'Юридическое лицо',
+                        ],
+                        'multiple' => false,
+                        'expanded' => true,
+                        'required' => true,
+                    ]
+                )
+                ->add('profileType', 'choice',
+                    [
+                        'choices' => [
+                            User::PROFILE_TYPE_CARRIER_AND_SENDER => 'Отправитель и перевозчик',
+                            User::PROFILE_TYPE_CARRIER => 'Перевозчик',
+                            User::PROFILE_TYPE_SENDER => 'Отправитель',
 
+                        ],
+                        'multiple' => false,
+                        'expanded' => true,
+                        'required' => true,
+                    ]
+                )
+                ->add('aboutMe', 'textarea', ['required' => false])
+                ->add('imgPath', 'file', ['required' => false, 'data_class' => null, 'mapped' => true])
+                // Блок настроек оповещения
+                ->add('smsBidCreateNotification', 'checkbox', ['required' => false])
+                ->add('smsBidAcceptNotification', 'checkbox', ['required' => false])
+                ->add('smsBidAgreeNotification', 'checkbox', ['required' => false])
+                ->add('smsBidRejectNotification', 'checkbox', ['required' => false])
+                ->add('smsBidRecallNotification', 'checkbox', ['required' => false])
+                ->add('smsShippingDeliverNotification', 'checkbox', ['required' => false])
+                ->add('smsShippingPayNotification', 'checkbox', ['required' => false])
+                ->add('smsShippingCompleteNotification', 'checkbox', ['required' => false])
+                ->add('smsShippingRefuseNotification', 'checkbox', ['required' => false])
+                ->add('smsShippingDisputeNotification', 'checkbox', ['required' => false])
+                ->add('smsShippingDebateNotification', 'checkbox', ['required' => false])
+                ->add('emailBidCreateNotification', 'checkbox', ['required' => false])
+                ->add('emailBidAcceptNotification', 'checkbox', ['required' => false])
+                ->add('emailBidAgreeNotification', 'checkbox', ['required' => false])
+                ->add('emailBidRejectNotification', 'checkbox', ['required' => false])
+                ->add('emailBidRecallNotification', 'checkbox', ['required' => false])
+                ->add('emailShippingDeliverNotification', 'checkbox', ['required' => false])
+                ->add('emailShippingPayNotification', 'checkbox', ['required' => false])
+                ->add('emailShippingCompleteNotification', 'checkbox', ['required' => false])
+                ->add('emailShippingRefuseNotification', 'checkbox', ['required' => false])
+                ->add('emailShippingDisputeNotification', 'checkbox', ['required' => false])
+                ->add('emailShippingDebateNotification', 'checkbox', ['required' => false])
+                ->add('gcmBidCreateNotification', 'checkbox', ['required' => false])
+                ->add('gcmBidAcceptNotification', 'checkbox', ['required' => false])
+                ->add('gcmBidAgreeNotification', 'checkbox', ['required' => false])
+                ->add('gcmBidRejectNotification', 'checkbox', ['required' => false])
+                ->add('gcmBidRecallNotification', 'checkbox', ['required' => false])
+                ->add('gcmShippingDeliverNotification', 'checkbox', ['required' => false])
+                ->add('gcmShippingPayNotification', 'checkbox', ['required' => false])
+                ->add('gcmShippingCompleteNotification', 'checkbox', ['required' => false])
+                ->add('gcmShippingRefuseNotification', 'checkbox', ['required' => false])
+                ->add('gcmShippingDisputeNotification', 'checkbox', ['required' => false])
+                ->add('gcmShippingDebateNotification', 'checkbox', ['required' => false]);
+        }
     }
 
     public function setDefaultOptions(OptionsResolverInterface $resolver)

4b17b13accbe9f8176a6b0f366062986365a50b7
Denis Manilo <info@tytymyty.ru>
2016-02-22 06:55:29 +0300

    Search

--- src/Binidini/SearchBundle/Controller/ShipmentController.php
+++ src/Binidini/SearchBundle/Controller/ShipmentController.php
@@ -13,6 +13,8 @@ namespace Binidini\SearchBundle\Controller;
 
 
 use Binidini\CoreBundle\Entity\Location;
+use Binidini\SearchBundle\Document\Shipment;
+use Binidini\SearchBundle\Document\ShipmentRepository;
 use Binidini\SearchBundle\Model\MyPagerfantaFactory;
 use Sylius\Bundle\ResourceBundle\Controller\ResourceController;
 use Symfony\Component\HttpFoundation\Request;
@@ -105,46 +107,34 @@ class ShipmentController extends ResourceController
 
     public function distanceAction(Request $request)
     {
-        $sort = $request->get('sort', 'delivery_datetime');
-        $latitude  = $request->get('lat', 55.7522);
-        $longitude = $request->get('lon', 37.6156);
         $searchAddress = $request->get('top-search', null);
 
-        //<-- Ведем базу передвижения пользователя
-        if (!is_null($longitude) && !is_null($latitude)) {
-            /** @var $user User */
-            $user = $this->getUser();
-
-            if (!is_null($user)) {
-
+        $latitude  = $request->get('lat');
+        $longitude = $request->get('lon');
+        //Ведем базу передвижения пользователя
+        /** @var $user User */
+        $user = $this->getUser();
+        if ($longitude && $latitude) {
+            if ($user) {
                 $loc = new Location();
-                $loc
-                    ->setUser($user)
+                $loc->setUser($user)
                     ->setLatitude($latitude)
-                    ->setLongitude($longitude)
-                ;
+                    ->setLongitude($longitude);
                 $this->getDoctrine()->getManager()->persist($loc);
-
                 $user->setLatitude($latitude);
                 $user->setLongitude($longitude);
-
                 $this->getDoctrine()->getManager()->flush();
             }
         }
-        // -->
-
-        #<-- Запрашиваем координаты адреса в яндекс картах
-        if ( !empty($searchAddress) ) {
-
+        //Запрашиваем координаты адреса в яндекс картах
+        if (!empty($searchAddress)) {
             $geocode = $this->container->get('binidini.geocode.yandex.client');
-
             $params = array(
                 'geocode' => $searchAddress,
-                'format'  => 'json',
+                'format' => 'json',
                 'results' => 1,
             );
-            $res = $geocode->get('?'.http_build_query($params, '', '&'))->send();
-
+            $res = $geocode->get('?' . http_build_query($params, '', '&'))->send();
             if ($res->getStatusCode() == 200) {
                 $response = json_decode($res->getBody(true));
                 if ($response->response->GeoObjectCollection->metaDataProperty->GeocoderResponseMetaData->found > 0) {
@@ -152,33 +142,52 @@ class ShipmentController extends ResourceController
                 }
             }
         }
-        # -->
-
-        $orderBy = [];
-        if ($sort == 'delivery_time') {
-            $orderBy = ['deliveryDatetime' => 'asc'];
-        } elseif ($sort == 'delivery_price') {
-            $orderBy = ['deliveryPrice' => 'desc'];
+        /** @var $repository ShipmentRepository */
+        $repository = $this->getRepository();
+        if (!$longitude || !$latitude) {
+            if ($user) {
+                $longitude = $user->getLongitude();
+                $latitude = $user->getLatitude();
+            }
+            if (!$longitude || !$latitude) {
+                $latitude = 59.9500;
+                $longitude = 30.3000;
+            }
         }
-
-        $shipments = $this->getRepository()->findBy([], $orderBy);
-
-        /** @var $shipment \Binidini\SearchBundle\Document\Shipment */
-        foreach ($shipments as $shipment) {
-
-            if (is_null($shipment->getPickupCoordinates())) {
-                $pickupDistance = 20000000;
+        $shipments = $repository->findByLocation($longitude, $latitude);
+        $shipments->setCurrentPage($request->get('page', 1));
+        $shipments->setMaxPerPage(10);
+        $result = array();
+        /** @var $shipment Shipment */
+        foreach ($shipments->getIterator() as $shipment) {
+            if ($shipment->getPickupCoordinates()) {
+                $latDiff = $shipment->getPickupCoordinates()->getLatitude() - $latitude;
+                $lonDiff = $shipment->getPickupCoordinates()->getLongitude() - $longitude;
+                $pickupDistance = sqrt(pow($latDiff, 2) + pow($lonDiff, 2));
             } else {
-                $pickupDistance = $this->circle_distance($latitude, $longitude, $shipment->getPickupCoordinates()->getLatitude(), $shipment->getPickupCoordinates()->getLongitude());
+                $pickupDistance = 0;
+            }
+            if ($shipment->getDeliveryCoordinates()) {
+                $latDiff = $shipment->getDeliveryCoordinates()->getLatitude() - $latitude;
+                $lonDiff = $shipment->getDeliveryCoordinates()->getLongitude() - $longitude;
+                $deliveryDistance = sqrt(pow($latDiff, 2) + pow($lonDiff, 2));
+            } else {
+                $deliveryDistance = 0;
             }
 
-            if (is_null($shipment->getDeliveryCoordinates())) {
-                $deliveryDistance = 20000000;
-            } else {
-                $deliveryDistance = $this->circle_distance($latitude, $longitude, $shipment->getDeliveryCoordinates()->getLatitude(), $shipment->getDeliveryCoordinates()->getLongitude());
-            }
-
-            $result[] = ['pickup_distance' => intval($pickupDistance), 'delivery_distance' => intval($deliveryDistance), 'order' => $shipment];
+            $result[] = [
+                'id' => $shipment->getId(),
+                'name' => $shipment->getName(),
+                'delivery_price' => $shipment->getDeliveryPrice(),
+                'guarantee' => $shipment->getGuarantee(),
+                'insurance' => $shipment->getInsurance(),
+                'pickup_address' => $shipment->getPickupAddress(),
+                'delivery_address' => $shipment->getDeliveryAddress(),
+                'payment_guarantee' => $shipment->getPaymentGuarantee(),
+                'delivery_datetime' => $shipment->getDeliveryDatetime(),
+                'delivery_distance' => intval(111000 * $deliveryDistance),
+                'pickup_distance' => intval(111000 * $pickupDistance),
+            ];
         }
         $view = $this
             ->view()
